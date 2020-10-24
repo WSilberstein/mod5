1c4c230d065aed4a9faac86b6d9ae3e64de57bdc
Mobyduck321 <mobyduck321@yahoo.com>
2020-10-23 21:01:13 -0500

    Calendar Complete

--- calendar/js/calendar.js
+++ calendar/js/calendar.js
@@ -1,6 +1,39 @@
 var dayNames = ["Sunday", "Monday", "Tuesday", "Wednsday", "Thursday", "Friday", "Saturday"];
+var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 var currentDate = Date.now();
+var currentMonth = new Date(Date.now()).getMonth();
+var currentYear = new Date(Date.now()).getFullYear();
 const calendar = document.getElementById('calendar-days');
+
+ $('#change_month_right').click(function() {
+     if(currentMonth == 11) {
+        currentMonth = 0;
+        currentYear++;
+     } else {
+        currentMonth++;
+     }
+    // console.log(currentMonth)
+    // console.log(currentYear)
+     calendar.innerHTML = '';
+     var month = new Month(currentYear, currentMonth);
+     month.initialize();
+     month.draw();
+ })
+
+ $('#change_month_left').click(function() {
+    if(currentMonth == 0) {
+       currentMonth = 11;
+       currentYear--;
+    } else {
+       currentMonth--;
+    }
+    //console.log(currentMonth)
+   // console.log(currentYear)
+    calendar.innerHTML = '';
+    var month = new Month(currentYear, currentMonth);
+    month.initialize();
+    month.draw();
+})
 
 function Day(date, events) {
     this.date = date;
@@ -19,6 +52,21 @@ function Day(date, events) {
         //Decrement the day by 1
         //Fetch events corresponding to the day
         //Return a new day given prev inputs
+    }
+
+    this.drawPadding = function(node) {
+        var container = document.createElement("div");
+        var numberContainer = document.createElement("div");
+        container.classList.add("card-day-padding");
+        container.classList.add("col-sm-1");
+        numberContainer.classList.add("day-number");
+        var dayNumber = document.createElement("span");
+        numberContainer.appendChild(dayNumber);
+
+        //Add events to day
+
+        container.appendChild(numberContainer);
+       return container
     }
 
     this.draw = function(node) {
@@ -58,8 +106,18 @@ function Week(days) {
     }
 
     this.draw = function(days) {
+      //  console.log(this.days)
+        //console.log((this.days[0].date))
         var row = document.createElement("div");
         row.classList.add('row');
+        if(new Date(this.days[0].date).getDay() != 0) {
+            for(var i = 0; i < new Date(this.days[0].date).getDay(); i++) {
+                //console.log("Create padding")
+                row.appendChild(this.days[0].drawPadding(row))
+            }
+            //console.log("DOES NOT START ON SUNDAY")
+        }
+        
         this.days.forEach(function(day) {
             row.appendChild(day.draw(row))
            // console.log(day.draw(row))
@@ -82,21 +140,34 @@ function Month(year, month) {
     this.initialize = function() {
 
         var date = new Date(year, month + 1, 0);
-        console.log(date);
+        //console.log(date);
         var daysReverse = [];
         var weekDays = [];
         var counter = 1;
         while (date.getMonth() === month) {
           daysReverse.push(new Date(date));
           //this.days.push(new Day(new Date(date)));
-          weekDays.push(new Day(new Date(date)));
+         /* weekDays.push(new Day(new Date(date)));
           if(counter != 1 && counter % 7 == 0) {
             this.weeks.push(weekDays);
             weekDays = [];
-          }
+          }*/
           date.setDate(date.getDate() - 1);
           counter++;
         }
+
+        for(var i = daysReverse.length - 1; i >= 0; i--) {
+           // console.log(daysReverse[i])
+            this.days.push(daysReverse[i])
+            weekDays.push(new Day(new Date(daysReverse[i])));
+            if(new Date(daysReverse[i]).getDay() == 6) {
+                //console.log(weekDays)
+                this.weeks.push(weekDays);
+                weekDays = [];
+            }
+        }
+
+
         this.weeks.push(weekDays);
        // console.log(this.weeks);
     }
@@ -117,69 +188,15 @@ function Month(year, month) {
         console.log("Drawing Calendar")
 
         var month = document.getElementById('month');
-        if(this.month == 0) {
-            month.innerText = "January";
-        } else if(this.month == 1) {
-            month.innerText = "February";
-        } else if(this.month == 2) {
-            month.innerText = "March";
-        } else if(this.month == 3) {
-            month.innerText = "April";
-        } else if(this.month == 4) {
-            month.innerText = "May";
-        } else if(this.month == 5) {
-            month.innerText = "June";
-        } else if(this.month == 6) {
-            month.innerText = "July";
-        } else if(this.month == 7) {
-            month.innerText = "August";
-        } else if(this.month == 8) {
-            month.innerText = "September";
-        } else if(this.month == 9) {
-            month.innerText = "October";
-        } else if(this.month == 10) {
-            month.innerText = "November";
-        } else if(this.month == 11) {
-            month.innerText = "December";
-        }
+        month.innerText = monthNames[this.month] + " " + this.year;
 
-        var daysOfWeek = document.getElementsByClassName('struct-day');
-        var firstDay = new Date(this.days[0].date).getDay();
-       // console.log(daysOfWeek[5]);
-        for(var i = 0; i < daysOfWeek.length; i++) {
-            var text = document.createElement("p")
-           // console.log(dayNames[i])
-         /*   if(firstDay == 0) {
-                text.innerText = "Sunday";
-            } else if(firstDay == 1) {
-                text.innerText = "Monday";
-            } else if(firstDay == 2) {
-                text.innerText = "Tuesday";
-            } else if(firstDay == 3) {
-                text.innerText = "Wednsday";
-            } else if(firstDay == 4) {
-                text.innerText = "Thursday";
-            } else if(firstDay == 5) {
-                text.innerText = "Friday";
-            } else if(firstDay == 6) {
-                text.innerText = "Saturday";
-            }*/
-            text.innerText = dayNames[i];
-            daysOfWeek[i].appendChild(text);
-          //  console.log(daysOfWeek[i])
-
-            firstDay++;
-            if(firstDay > 6) {
-                firstDay = 0;
-            }
-        }
         
 
         this.weeks.forEach(function(week) {
-            var weekObj = new Week(week);
-            //console.log(weekObj)
-            weekObj.draw();
-           
+            if(week.length > 0) {
+                var weekObj = new Week(week);
+                weekObj.draw();
+            }
         })
     }
 
